e4ff261ea6cfa8b9b52d62049d45855c7a4b89f3
Somadina <kingswatter@gmail.com>
2025-06-02 19:58:43 +0100

    init auth process

---
+++ src/routes.ts
@@ -0,0 +1,27 @@
+/**
+ * An array of public routes that are allowed to be accessed without authentication.
+ * These routes will redirect logged-in users to /settings.
+ * @type {string[]}
+ */
+
+export const publicRoutes: string[] = ['/'];
+
+/**
+ * An array of routes used for authentication.
+ * These routes will redirect logged-in users to /settings.
+ * @type {string[]}
+ */
+export const authRoutes: string[] = ['/auth/login', '/auth/register'];
+
+/**
+ * The prefix used for API authentication routes.
+ * The routes that start with this prefix are used for auth purposes
+ * @type {string}
+ */
+export const apiAuthPrefix: string = '/api/auth';
+
+/**
+ * The default redirect path after a successful login.
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT: string = '/settings';
