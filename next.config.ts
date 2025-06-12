0b63bfcc7c89b75536c21629366a87e3bf3f9c58
Somadina <kingswatter@gmail.com>
2025-06-12 23:06:05 +0100

    some fixes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from 'next';
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 
 const nextConfig: NextConfig = {
 	/* config options here */
-	webpack(config) {
+	webpack(config, { isServer }) {
+		if (isServer) {
+			config.plugins = [...config.plugins, new PrismaPlugin()];
+		}
+
 		config.module.rules.push({
 			test: /\.svg$/,
 			use: ['@svgr/webpack'],
